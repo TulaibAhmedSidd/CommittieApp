bf259402505970abe10f1417b0c3bd0f6d3219db
TulaibAhmedSidd <ahsidtullu@gmail.com>
2026-02-04 20:18:13 +0500

    Work for build error

--- app/userDash/page.tsx
+++ app/userDash/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 import MainPage from "../Components/MainPage";
 
 const UserDash = () => {
   return (
-    <div className="">
+    <Suspense fallback={<div className="flex flex-col items-center justify-center min-h-[400px] gap-6 animate-in fade-in duration-500"><div className="w-16 h-16 border-4 border-primary-500 border-t-transparent rounded-2xl animate-spin shadow-lg" /></div>}>
       <MainPage />
-    </div>
+    </Suspense>
   );
 };
 
